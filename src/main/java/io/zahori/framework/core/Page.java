40d015c5875d63df8251648748c6128481fa1e4a
David Vazquez <davidvazqueznovella@gmail.com>
2023-04-19 16:35:03 +0200

    fix iframes: remove reset to default content

--- src/main/java/io/zahori/framework/core/Page.java
+++ src/main/java/io/zahori/framework/core/Page.java
@@ -12,25 +12,18 @@ package io.zahori.framework.core;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
-
-
 import io.zahori.framework.utils.Chronometer;
 import io.zahori.framework.utils.Pause;
-import org.openqa.selenium.*;
-
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -39,6 +32,9 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Page implements Serializable {
 
@@ -75,10 +71,12 @@ public class Page implements Serializable {
         return this.testContext.driver.getTitle();
     }
 
-    public void switchToFrame(String frameName) {
-        PageElement frame = new PageElement(this, "iFrame temporal webElement", Locator.id(frameName));
-        // frame.initWebElement();
-        // driver.switchTo().frame(frame.webElement);
+    public void switchToFrame(String frameId) {
+        switchToFrame(Locator.id(frameId));
+    }
+
+    public void switchToFrame(Locator locator) {
+        PageElement frame = new PageElement(this, "iFrame temporal webElement", locator);
         Chronometer crono = new Chronometer();
         this.switchToFrameWebElement(crono, frame);
     }
@@ -147,7 +145,7 @@ public class Page implements Serializable {
             if (!"".equals(winHandle) && !parentWindowHandler.equals(winHandle)) {
                 existChildWindow = true;
                 this.driver.switchTo().window(winHandle); // switch to popup
-                                                          // window
+                // window
                 this.driver.switchTo().window(parentWindowHandler);
             }
         }
@@ -230,18 +228,7 @@ public class Page implements Serializable {
     }
 
     public List<PageElement> getPageElements(Locator locator) {
-        return this.getPageElements(null, locator);
-    }
-
-    public List<PageElement> getPageElements(Locator[] frameLocators, Locator locator) {
         List<PageElement> elementsList = new ArrayList<>();
-        if ((frameLocators != null) && (frameLocators.length > 0)) {
-            for (Locator actualFrame : frameLocators) {
-                switchToFrame(new PageElement(this, "frameElement", actualFrame));
-            }
-        }
-
-
 
         WebElement element = null;
         try {
@@ -254,7 +241,7 @@ public class Page implements Serializable {
         List<WebElement> wdElements = this.driver.findElements(locator.getBy());
 
         for (WebElement each : wdElements) {
-            PageElement pageElement = new PageElement(this, frameLocators, "pageElement", locator);
+            PageElement pageElement = new PageElement(this, "pageElement", locator);
             pageElement.webElement = each;
             elementsList.add(pageElement);
         }
