ea1ecdc9e2f95d29ab122ccc7ac07d0079ec6b59
David Vazquez <davidvazqueznovella@gmail.com>
2023-06-27 11:36:06 +0200

    Disable opentelemetry tracing

--- src/main/java/io/zahori/framework/driver/RemoteDriver.java
+++ src/main/java/io/zahori/framework/driver/RemoteDriver.java
@@ -12,37 +12,36 @@ package io.zahori.framework.driver;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.zahori.framework.driver.browserfactory.Browsers;
+import java.util.HashMap;
+import java.util.Map;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.AbstractDriverOptions;
 import org.openqa.selenium.remote.LocalFileDetector;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RemoteDriver implements Driver {
 
     @Override
-    public WebDriver getDriver(Browsers browsers){
+    public WebDriver getDriver(Browsers browsers) {
         AbstractDriverOptions<?> options = getOptions(browsers);
         final int timeoutSecondsForDriverCreation = 1200; // 1200seconds = 20 minutes
-        WebDriver webDriver = WebDriverManager.getInstance(browsers.getName().toUpperCase()).browserVersion(browsers.getVersion()).remoteAddress(browsers.getRemoteUrl()).capabilities(options).timeout(timeoutSecondsForDriverCreation).create();
-        
-        ((RemoteWebDriver)webDriver).setFileDetector(new LocalFileDetector());
+        WebDriver webDriver = WebDriverManager.getInstance(browsers.getName().toUpperCase()).browserVersion(browsers.getVersion()).remoteAddress(browsers.getRemoteUrl()).capabilities(options).timeout(timeoutSecondsForDriverCreation).disableTracing().create();
+
+        ((RemoteWebDriver) webDriver).setFileDetector(new LocalFileDetector());
         webDriver.manage().window().maximize();
-        
+
         return webDriver;
     }
 
